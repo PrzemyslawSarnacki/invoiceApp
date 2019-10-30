e8119987f2f0689620c287c6c1f1d14997071fb0
PrzemyslawSarnacki <przemos100@gmail.com>
2019-10-30 15:46:44 +0100

    First Commit

--- fullTextSearchInMongo.py
+++ fullTextSearchInMongo.py
@@ -1,5 +1,7 @@
 import pymongo
 import re
+from bson import ObjectId
+
 
 #Search for item in certain collection 
 
@@ -12,16 +14,44 @@ collection = database.ASOR
 # in collection(NAZWA in this case)
 # collection.create_index([('NAZWA', 'text')])
 
-
+ 
 def searchForItem(search_this):
     collection.find({"$text": {"$search": search_this}})
     for item in collection.find({"$text": {"$search": search_this}}).limit(1): 
         print(item)
-        print(item.get("KOSZT"))
         return(item.get("KOSZT")), (item.get("NAZWA"))
 
 
-search_this = "promocja"
+def update_or_create(document_id, data):
+    document = collection.update_one({'_id': ObjectId(document_id)}, {"$set": data}, upsert=True)
+    return document.acknowledged
 
+
+def getSingleData(document_id):
+    data = collection.find_one({'_id': ObjectId(document_id)})
+    return data
+
+
+def getMultipleData():
+    data = collection.find().limit(10)
+    return list(data)
+
+
+def updateData(document_id, data):
+    document = collection.update_one({'_id': ObjectId(document_id)}, {"$set": data})
+    return document.acknowledged
+
+
+def insertData(data):
+    document = collection.insert_one(data)
+    return document.inserted_id
+
+
+def removeData(document_id):
+    document = collection.delete_one({'_id': ObjectId(document_id)})
+    return document.acknowledged
+
+search_this = "promocja"
 dbprice, dbname = searchForItem(search_this)
-print(dbname)
+# print(dbname)
+print(getMultipleData())
