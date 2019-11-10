339500c9c3584bbb272ec6ef6d8036762966d8d3
PrzemyslawSarnacki <przemos100@gmail.com>
2019-11-10 18:12:30 +0100

    Choosing Client Works

--- invoiceApp.py
+++ invoiceApp.py
@@ -8,6 +8,7 @@ import icons_rc
 # invoice = generateInvoice.setClient('Adfasd', 'efFdsf', 'AFAAF', '')
 
 class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
+    invoice = None
 
     def __init__(self):
         super(PythonMongoDB, self).__init__()
@@ -39,28 +40,11 @@ class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
         self.tableView_2.setItemDelegate(self.delegate)
         self.tableView_2.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.tableView_2.customContextMenuRequested.connect(lambda : self.context_menu_client(self.model_2, self.tableView_2))
-        self.generateInvoiceButton.clicked.connect(lambda : generateInvoice.createInvoice(invoice))
+        # self.generateInvoiceButton.clicked.connect(lambda : print(j)))
+        self.generateInvoiceButton.clicked.connect(lambda : generateInvoice.createInvoice(PythonMongoDB.invoice))
         self.tableView_3.setModel(self.model_3)
         self.tableView_3.setItemDelegate(self.delegate)
 
-    def context_menu(self, varModel, varTableView):
-        menu = QtWidgets.QMenu()
-        refresh_data = menu.addAction("Refresh Data")
-        refresh_data.triggered.connect(lambda: varModel.getMultipleData(varTableView.currentIndex()))
-        add_to_invoice = menu.addAction("Add This To Invoice")
-        add_to_invoice.setIcon(QtGui.QIcon(":/icons/images/add-icon.png"))
-        add_to_invoice.triggered.connect(lambda: varModel.addRowsToInvoice(varTableView.currentIndex(), invoice))
-        add_data = menu.addAction("Add New Data")
-        add_data.setIcon(QtGui.QIcon(":/icons/images/add-icon.png"))
-        add_data.triggered.connect(lambda: varModel.insertRows())
-        if varTableView.selectedIndexes():
-            remove_data = menu.addAction("Remove Data")
-            remove_data.setIcon(QtGui.QIcon(":/icons/images/remove.png"))
-            remove_data.triggered.connect(lambda: varModel.removeRows(varTableView.currentIndex()))
-        cursor = QtGui.QCursor()
-        menu.exec_(cursor.pos())
-    
-    
     def context_menu_client(self, varModel , varTableView):
         menu = QtWidgets.QMenu()
         set_client_for_invoice = menu.addAction("Choose this Client")
@@ -68,7 +52,7 @@ class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
         x = lambda : (varModel.setClientForInvoice(varTableView.currentIndex()))
         set_client_for_invoice.triggered.connect(x)
         y = x()
-        invoice = generateInvoice.setClient(y, 'clientAddress', 'clientContact', '')
+        PythonMongoDB.invoice = generateInvoice.setClient(y, 'clientAddress', 'clientContact', '')
         debug = menu.addAction("Debug")
         debug.setIcon(QtGui.QIcon(":/icons/images/add-icon.png"))
         debug.triggered.connect(lambda: print(y))
@@ -81,7 +65,26 @@ class PythonMongoDB(main.Ui_MainWindow, QtWidgets.QMainWindow):
             remove_data.triggered.connect(lambda: varModel.removeRows(varTableView.currentIndex()))
         cursor = QtGui.QCursor()
         menu.exec_(cursor.pos())
-        return invoice
+        return PythonMongoDB.invoice
+
+    def context_menu(self, varModel, varTableView):
+        menu = QtWidgets.QMenu()
+        refresh_data = menu.addAction("Refresh Data")
+        refresh_data.triggered.connect(lambda: varModel.getMultipleData(varTableView.currentIndex()))
+        add_to_invoice = menu.addAction("Add This To Invoice")
+        add_to_invoice.setIcon(QtGui.QIcon(":/icons/images/add-icon.png"))
+        add_to_invoice.triggered.connect(lambda: varModel.addRowsToInvoice(varTableView.currentIndex(), PythonMongoDB.invoice))
+        add_data = menu.addAction("Add New Data")
+        add_data.setIcon(QtGui.QIcon(":/icons/images/add-icon.png"))
+        add_data.triggered.connect(lambda: varModel.insertRows())
+        if varTableView.selectedIndexes():
+            remove_data = menu.addAction("Remove Data")
+            remove_data.setIcon(QtGui.QIcon(":/icons/images/remove.png"))
+            remove_data.triggered.connect(lambda: varModel.removeRows(varTableView.currentIndex()))
+        cursor = QtGui.QCursor()
+        menu.exec_(cursor.pos())
+    
+    
         
 
 if __name__ == '__main__':
